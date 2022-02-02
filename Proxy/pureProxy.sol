108e1fd90b5ea2df6a4e50409f13f7d77ec44fb4
b69liu <b69liu@uwaterloo.ca>
2022-02-01 23:28:32 -0500

    use free mem slot to store calldata

--- Proxy/pureProxy.sol
+++ Proxy/pureProxy.sol
@@ -16,10 +16,14 @@ contract Proxy{
         // load target contract address to stack
         address targetContract = myContract;
         assembly{
+            // https://docs.soliditylang.org/en/v0.8.10/internals/layout_in_memory.html#layout-in-memory
+            // get free pointer, initially it should point to 0x80
+            let p := mload(0x40)
             // load calldata to memory
-            calldatacopy(0, 0, calldatasize())
+            calldatacopy(p, 0, calldatasize())
+            mstore(0x40, add(p, calldatasize()))  // move free pointer forward (pointing to 0x80+size)
             // delegatecall to the target contract with the loaded calldata in memory
-            let result := delegatecall(gas(), targetContract, 0, calldatasize(), 0x0, 0)
+            let result := delegatecall(gas(), targetContract, 0x80, calldatasize(), 0x0, 0)
             // copy returned value to memory
             returndatacopy(0, 0, returndatasize())
             // check if the delegatecall successed, and revert if it failed
